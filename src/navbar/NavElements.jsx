a40cd09784db75f980cd51603421f2caa4113c6b
pratham0543 <pratham0543.be20@chitkara.edu.in>
2022-09-05 09:16:13 +0530

    Navbar created with functionality of react made navElements and Login modal

---
+++ src/navbar/NavElements.jsx
@@ -0,0 +1,9 @@
+const NavElements=(props)=>{
+    return(
+        <li className="nav-item">
+        <a className="nav-link "  href="#">{props.text}</a>
+      </li>
+    )
+}
+
+export default NavElements;
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,55 +1,44 @@
 import './navbar.css';
+import LoginModal from './LoginModal';
+import NavElements from './NavElements';
 function Navbar(){
-
 return(
-    <div>
+    <div className="main">
     <nav className="navbar bg-light">
   <div className="container-fluid ">
     <a className="navbar-brand" href="./index.html">
     <i className="fa-solid fa-dumbbell me-2"></i>
        <span> FITOLOGY</span>
     </a>
-    <button id="login-btn" className="btn btn-primary">Login/Sign Up</button>
+    <button type="button" id="login-btn" className="btn btn-primary login-button" data-bs-toggle="modal" data-bs-target="#ModalForm">Login/Sign Up</button>
+
+      {/* login modal */}
+       <LoginModal/>
+  {/* login modal finished */}
   </div>
 </nav>
 <nav className="navbar navbar-expand-lg nav-content">
   <div className="container-fluid">
     
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+    <button className="icon-button-toggle navbar-toggler toggle_button" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span><i className="fa-solid fa-bars"></i></span>
     </button>
     <div className="collapse navbar-collapse nav-ul" id="navbarNav">
       <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link "  href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Nutrition</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Sports</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Plans</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link " href="#">Exercises</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link " href="#">Blogs</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link " href="#">Contact Us</a>
-        </li>
+        {/* Provide with href links in props */}
+       <NavElements text="Home"/>
+       <NavElements text="Nutriton"/>
+       <NavElements text="Sports"/>
+        <NavElements text="Plans"/>
+        <NavElements text="Exercises"/>
+        <NavElements text="Blogs"/>
+        <NavElements text="Contact Us"/>
       </ul>
     </div>
   </div>
 </nav>
 </div>
+)}
 
-)
-
-
-}
 
 export default Navbar;
